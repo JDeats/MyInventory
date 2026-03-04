5ec456ae496a356fe8ecb37a251cc5ed3b7dc77c
DEATSPC\JDeat <JDeaton26@gmail.com>
2026-03-03 18:33:33 -0800

    Restyled the menu bar

--- client/src/app/layout/MenuBar.tsx
+++ client/src/app/layout/MenuBar.tsx
@@ -1,47 +1,48 @@
-import { Button } from "@mui/material";
-import { Menu, MenuButton, MenuDivider, MenuHeader, MenuItem, SubMenu } from "@szhsin/react-menu";
+import { Box, Button } from "@mui/material";
+import GiteIcon from "@mui/icons-material/Gite";
+import { Menu, MenuDivider, MenuItem, SubMenu } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 import "./styles.css";
 
 export default function MenuBar() {
   return (
-    <>
-        <Menu
-            transition 
-            menuButton={
-                <Button className="menu-button" variant="contained">File</Button>
-        }>
-            <SubMenu label="New...">
-                <SubMenu label="Electronic Component...">
-                    <MenuItem onClick={() => {}}>Capacitor</MenuItem>
-                </SubMenu>
-            </SubMenu>
-            <MenuItem>New File</MenuItem>
-            <MenuItem>Save</MenuItem>
-            <MenuItem>Close Window</MenuItem>
-            <MenuDivider />
-            <MenuHeader>Edit</MenuHeader>
-            <MenuItem>Cut</MenuItem>
-            <MenuItem>Copy</MenuItem>
-            <MenuItem>Paste</MenuItem>
-            <MenuDivider />
-            <MenuItem>Print</MenuItem>
-        </Menu>
-        <Menu menuButton={<MenuButton>Menu</MenuButton>}>
-            <MenuItem>New File</MenuItem>
-            <SubMenu label="Edit">
-                <MenuItem>Cut</MenuItem>
-                <MenuItem>Copy</MenuItem>
-                <MenuItem>Paste</MenuItem>
-                <SubMenu label="Find">
-                    <MenuItem>Find...</MenuItem>
-                    <MenuItem>Find Next</MenuItem>
-                    <MenuItem>Find Previous</MenuItem>
-                </SubMenu>
-            </SubMenu>
-            <MenuItem>Print...</MenuItem>
-        </Menu>
-    </>
-  )
+    <Box sx={{ margin: "1rem", display: "flex", gap: "2px" }}>
+      <GiteIcon sx={{ fontSize: "48px" }} />
+      <Menu
+        transition
+        menuButton={
+          <Button
+            className="menu-button"
+            variant="contained"
+            sx={{ marginLeft: "20px" }}
+          >
+            File
+          </Button>
+        }
+      >
+        <SubMenu label="New...">
+          <SubMenu label="Electronic Component...">
+            <MenuItem onClick={() => {}}>Capacitor</MenuItem>
+          </SubMenu>
+        </SubMenu>
+        <MenuItem>New File</MenuItem>
+        <MenuDivider />
+        <MenuItem>New File</MenuItem>
+      </Menu>
+      <Menu
+        transition
+        menuButton={
+          <Button className="menu-button" variant="contained">
+            Category
+          </Button>
+        }
+      >
+        <MenuItem onClick={() => {}}>Capacitor</MenuItem>
+        <MenuItem onClick={() => {}}>Ferrite Beads</MenuItem>
+        <MenuItem onClick={() => {}}>Other</MenuItem>
+        <MenuItem onClick={() => {}}>Resistor</MenuItem>
+      </Menu>
+    </Box>
+  );
 }
